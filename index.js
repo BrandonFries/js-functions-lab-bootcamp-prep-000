9d1ee59e68532c6308f93865ffdc31200ce260d3
Brandon Fries <brandonfries1993@gmail.com>
2019-06-02 13:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // write your code below!
-function happyHolidays()
+function happyHolidays(string) {
+  return ('Happy Holidays!')
+}
+function happyHolidaysTo(string) {
+  return ('Happy holidays, ${name}!')
+}
